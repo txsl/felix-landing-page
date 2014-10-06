d8b42060f2e20a681085aa657528c4f474d5e871
Thomas Lim <txl11@ic.ac.uk>
2014-10-06 20:19:59 +0100

    Updated ajax code so it's in line with the new way of sending data

--- ajax.php
+++ ajax.php
@@ -6,26 +6,29 @@ header('Content-Type: application/json');
 
 $email = $_POST['email'];
 
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
- 	$response = json_encode(array("success" => False, "reason" => 'Invalid email address')); 
+if(strcmp($_POST['want_to_help'], "true"))
+{
+	$involved = True;
+}
+else
+{
+	$involved = False;
 }
 
 $email = $con->real_escape_string($email);
 $user_agent = $con->real_escape_string($_SERVER['HTTP_USER_AGENT']);
 $user_ip = $con->real_escape_string($_SERVER['REMOTE_ADDR']);
 
-if($con->query("INSERT into emails (address, ip, user_agent) VALUES ('$email', '$user_ip', '$user_agent')")){
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ 	$response = json_encode(array("success" => False, "reason" => 'Invalid email address')); 
+}
+
+
+if($con->query("INSERT into emails (address, involved, ip, user_agent) VALUES ('$email', '$involved', '$user_ip', '$user_agent')")){
 	$response = json_encode(array("success" => True,));	
 }
 else{
 	$response = json_encode(array("success" => False, "reason" => 'You appear to have already signed up'));
-}
-
-$sections = ['news', 'comments', 'science', 'politics', 'business', 'arts', 'music', 'film', 'fashion', 'games',
-			'tech', 'travel', 'food', 'tv', 'books', 'sports']
-
-if ($_GET['all'] == 'yes'){
-	
 }
 
 echo $response;
